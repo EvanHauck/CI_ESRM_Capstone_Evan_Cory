f4017b26d08498187c5cc336cc5f44cbbdb67ab8
Evan Hauck <evan.hauck5@gmail.com>
2017-02-15 18:48:35 -0800

    First Commit
    Basic start to the project

---
+++ ESRM_SierraWeather/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class EsrmSierraweatherConfig(AppConfig):
+    name = 'ESRM_SierraWeather'
---
+++ ESRM_SierraWeather/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Record(models.Model):
+    recordNum = models.BigAutoField(primary_key=True)
+    recordDate = models.DateTimeField()
+    battAvg = models.DecimalField(max_digits=4,decimal_places=2)
+    pTempCAvg = models.DecimalField(max_digits=4,decimal_places=2)
+    airTCAvg = models.DecimalField(max_digits=4,decimal_places=2)
+    rH = models.DecimalField(max_digits=4,decimal_places=2)
+    slrkW = models.DecimalField(max_digits=4,decimal_places=2)
+    slrMJTot = models.DecimalField(max_digits=8,decimal_places=6)
+    wSMs = models.DecimalField(max_digits=6,decimal_places=3)
+    windDir = models.DecimalField(max_digits=5,decimal_places=1)
+    pARDen = models.IntegerField()
+    pARTotTot = models.DecimalField(max_digits=10,decimal_places=2)
+    bPMmHg = models.DecimalField(max_digits=6,decimal_places=1)
+    rainMmTot = models.IntegerField()
+
+    class Meta:
+        ordering = ['-recordNum']
+
+    def __str__(self):
+        return self.recordNum
+
