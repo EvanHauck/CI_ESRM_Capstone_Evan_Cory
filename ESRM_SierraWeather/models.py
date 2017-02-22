fb82708256685e31ea8e907acc100b9b72e8dad8
Evan Hauck <evan.hauck5@gmail.com>
2017-02-22 09:51:51 -0800

    updated data type for primary key

--- ESRM_SierraWeather/models.py
+++ ESRM_SierraWeather/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Record(models.Model):
-    recordNum = models.BigAutoField(primary_key=True)
+    recordNum = models.IntegerField(primary_key=True)
     recordDate = models.DateTimeField()
     battAvg = models.DecimalField(max_digits=4,decimal_places=2)
     pTempCAvg = models.DecimalField(max_digits=4,decimal_places=2)
