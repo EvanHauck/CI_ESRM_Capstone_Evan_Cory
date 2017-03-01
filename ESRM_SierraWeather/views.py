14cd97d0587af8d7878ab66c48608ef438785b65
zoothar <cory.gomez541@myci.csuci.edu>
2017-03-01 10:32:55 -0800

    some stuff that doesnt work yet

--- ESRM_SierraWeather/views.py
+++ ESRM_SierraWeather/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+from django.template import Context, loader
+
+def mainpage(request):
+    template = "ESRM_SierraWeather/templates/Main_Page.html"
+    return render(request,template)
